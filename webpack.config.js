a49ae7c9a7d2defb316f2152906d36e0cf8dce57
Vincent Derks <vincent.derks@webengine.nl>
2016-12-09 17:42:53 +0100

    [Changed] Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,81 @@
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: './web/',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    babelrc: false,
-                    presets: [
-                        'es2015',
-                        'stage-0',
-                        'react'
-                    ],
-                    plugins: [
-                        'transform-decorators-legacy',
-                        'transform-object-rest-spread'
-                    ]
-                }
-            }
-        ],
+const prod = process.argv.indexOf('-p') !== -1;
 
+if(prod){
+
+    var webpack = require('webpack');
+
+    module.exports = {
+        entry: './src/index.js',
+        output: {
+            path: './web/',
+            filename: 'bundle.js'
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel',
+                    query: {
+                        babelrc: false,
+                        presets: [
+                            'es2015',
+                            'stage-0',
+                            'react'
+                        ],
+                        plugins: [
+                            'transform-decorators-legacy',
+                            'transform-object-rest-spread'
+                        ]
+                    }
+                }
+            ],
+
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+              'process.env':{
+                'NODE_ENV': JSON.stringify('production')
+              }
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+              compress:{
+                warnings: true
+              }
+            }),
+        ]
     }
+
+} else {
+
+    module.exports = {
+        entry: './src/index.js',
+        output: {
+            path: './web/',
+            filename: 'bundle.js'
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel',
+                    query: {
+                        babelrc: false,
+                        presets: [
+                            'es2015',
+                            'stage-0',
+                            'react'
+                        ],
+                        plugins: [
+                            'transform-decorators-legacy',
+                            'transform-object-rest-spread'
+                        ]
+                    }
+                }
+            ],
+
+        }
+    }
+
 }
